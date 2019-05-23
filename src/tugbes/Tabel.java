32d21c9cc0363a32451dc0cf5f23ee418c237172
User <User@172.16.100.177>
2019-05-23 06:39:28 +0000

    Commit bro

---
+++ src/tugbes/Tabel.java
@@ -0,0 +1,13 @@
+package tugbes;
+
+/**
+ *
+ * @author User
+ */
+public class Tabel {
+    public static void main(String[] args) {
+        TuBes t = new TuBes ();
+        t.setVisible(true);
+    }
+
+    }
